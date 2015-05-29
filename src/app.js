e2a44f564d85d5bbeafb72d3a40924ef05638980
brobostigon <scraliontis@gmail.com>
2015-05-29 18:38:01 +0100

    v0.1

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var UI = require('ui');
+
+var Battery = navigator.battery || navigator.webkitBattery ||
+		navigator.mozBattery || navigator.msBattery;
+
+var detailCard = new UI.Card({
+
+  console.log('Level: ' + Math.floor(Battery.level * 100) + '%'); // 30%
+  console.log('Charging: ' + Battery.charging);                   // true
+	
+detailCard.show();
