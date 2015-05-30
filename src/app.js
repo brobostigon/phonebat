5217193d9d6708b3999fdaadc72140ee911143fc
brobostigon <scraliontis@gmail.com>
2015-05-30 13:06:59 +0100

    fix1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,5 @@ var detailCard = new UI.Card({
 
   console.log('Level: ' + Math.floor(Battery.level * 100) + '%'); // 30%
   console.log('Charging: ' + Battery.charging);                   // true
-	
+});
 detailCard.show();
