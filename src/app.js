f4195b766210e51909d18a4a953d9a2ad4fb47c3
brobostigon <scraliontis@gmail.com>
2015-06-02 15:09:10 +0100

    v.0.1 partly working shows text

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
 var UI = require('ui');
 //var Vector2 = require('vector2');
 
-
-//var wind = new UI.Window();
-
-
 var Battery = navigator.battery || navigator.webkitBattery ||
 		navigator.mozBattery || navigator.msBattery;
 
-
-var card = UI.Card(	
-	console.log('Level: ' + Math.floor(Battery.level * 100) + '%');
-  console.log('Charging: ' + Battery.charging);
-)
-
+var card = new UI.Card({
+	title: 'boots',
+	body: 'black',
+});
+card.title(Battery.charging);
+card.body(Battery.level);
 card.show();
-
-wind.on('show', function() {
+	//text = Battery.level;
+//console.log('Level: ' + Math.floor(Battery.level * 100) + '%')),
+  //console.log('Charging: ' + Battery.charging)),
 	
-wind.show();
