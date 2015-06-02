3be2ba333c171764fa089ca3f41d501290f3bd3b
brobostigon <scraliontis@gmail.com>
2015-06-02 16:20:41 +0100

    added cards.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 var UI = require('ui');
 //var Vector2 = require('vector2');
-
+var Clock = require('clock');
 var Battery = navigator.battery || navigator.webkitBattery ||
 		navigator.mozBattery || navigator.msBattery;
 
 var card = new UI.Card({
-	title: 'boots',
-	body: 'black',
+	title: '',
+	body: '',
 });
-card.title(Battery.charging);
-card.body(Battery.level);
+
+var cl1 = Clock.weekday.hour;
+var cl2 = Clock.weekday.minutes;
+card.title(cl2);
+card.body(cl1);
 card.show();
 	//text = Battery.level;
 //console.log('Level: ' + Math.floor(Battery.level * 100) + '%')),
